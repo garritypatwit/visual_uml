4064039ebdcc1bfa0a7dc160c29c7f408a1ed066
Aria Hogan <70959736+sandersona2@users.noreply.github.com>
2024-03-29 12:18:30 -0400

    remove init flask

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const VisualUML());
-}
-
-class BackendBloc extends Cubit<String> {
-  BackendBloc() : super('');
-
-  Future<void> fetchData() async {
-    final response = await http.get(Uri.parse('http://localhost:5000/api/data'));
-    if (response.statusCode == 200) {
-      emit(response.body);
-    } else {
-      emit('Failed to fetch data');
-    }
-  }
 }
 
 class VisualUML extends StatelessWidget {
