9d9413115643fdc62ab2d28267912f938e70b6bc
garritypatwit <garrityp@wit.edu>
2024-03-31 16:11:28 -0400

    Moved functions from model to controller

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,5 +1,45 @@
 import 'model.dart';
 
 class DiagramController {
-  
+  Diagram diagram = Diagram();
+
+  void addLifeline(String title) {
+    diagram.lifelines.add(Lifeline(title: title));
+  }
+
+  void insertLifeline(int index, String title) {
+    diagram.lifelines.insert(index, Lifeline(title: title));
+  }
+
+  void removeLifeline(int index) {
+    diagram.lifelines.removeAt(index);
+  }
+
+  int lifelineCount() {
+    return diagram.lifelines.length;
+  }
+
+  Lifeline getLifeline(int index) {
+    return diagram.lifelines[index];
+  }
+
+  void addConnection(int srcIndex, int destIndex) {
+    diagram.connections.add(Connection(src: diagram.lifelines[srcIndex], dest: diagram.lifelines[destIndex]));
+  }
+
+  void insertConnection(int index, int srcIndex, int destIndex) {
+    diagram.connections.insert(index, Connection(src: diagram.lifelines[srcIndex], dest: diagram.lifelines[destIndex]));
+  }
+
+  void removeConnection(int index) {
+    diagram.connections.removeAt(index);
+  }
+
+  int connectionCount() {
+    return diagram.connections.length;
+  }
+
+  Connection getConnection(int index) {
+    return diagram.connections[index];
+  }
 }
--- lib/model.dart
+++ lib/model.dart
@@ -1,80 +1,30 @@
-import 'package:flutter/services.dart';
-
 class Diagram {
   Diagram({
-     List<Lifeline>? lifelines ,
-     List<Connection>? connections,
-  }) : lifelines = lifelines ?? [],
-       connections =  connections ?? [];
+    List<Lifeline>? lifelines,
+    List<Connection>? connections,
+  })  : lifelines = lifelines ?? [],
+        connections = connections ?? [];
 
   List<Lifeline> lifelines;
   List<Connection> connections;
-
-  void addLifeline(String title, int index) {
-    lifelines.insert(index, Lifeline(title: title, index: index));
-    return;
-  }
-
-  void removeLifeline(int index) {
-    lifelines.removeAt(index); 
-    return;
-  }
-
-  Lifeline getLifeline(index) {
-    return lifelines.elementAt(index);
-  }
-
-  void addConnection(int index, Lifeline src, Lifeline dest){
-    connections.insert(index, Connection(src: src, dest: dest));
-    return;
-  }
-
-  void removeConnection(int index) {
-    connections.removeAt(index); 
-    return;
-  }
-
-
 }
 
 class Lifeline {
   Lifeline({
     required this.title,
-    required this.index,
   });
+
   String title;
-  int index;
-
-  String getTitle(){
-    return title;
-  }
-
-  int getIndex(){
-    return index;
-  }
 }
 
-class Connection { 
+class Connection {
   Connection({
     required this.src,
     required this.dest,
     this.title = '',
-
   });
+
   String title;
   Lifeline src;
   Lifeline dest;
-
-   String getTitle(){
-    return title;
-  }
-
-  int getSrcIndex(){
-    return src.getIndex();
-  }
-
-  int getDestIndex(){
-    return dest.getIndex();
-  }
-
 }
